1d97aca3f02b991ca7919e658e6ce843ea16ca6a
Sachin Prajapati <53299703+sachinprajapati8604@users.noreply.github.com>
2021-08-31 01:06:17 +0530

    footer updated

--- src/component/Footer/Footer.js
+++ src/component/Footer/Footer.js
@@ -11,7 +11,7 @@ export default function Footer() {
                 <li> <a target="_blank" rel="noreferrer" id="linkedin" href="https://www.linkedin.com/in/sachin-prajapati-8b1993188/" ><FaLinkedinIn /></a> </li>
                 <li> <a target="_blank" rel="noreferrer" id="github" href="https://github.com/sachinprajapati8604"><FaGithub /> </a> </li>
                 <li> <a target="_blank" rel="noreferrer" id="facebook" href="https://www.facebook.com/bullet.raja.1998"><FaFacebookF />  </a> </li>
-                <li> <a target="_blank" rel="noreferrer" id="insta" href="https://www.instagram.com/prajapati_rangbaz/?r=nametag">  <FaInstagram /></a> </li>
+                <li> <a target="_blank" rel="noreferrer" id="insta" href="https://www.instagram.com/_sachin__prajapati___">  <FaInstagram /></a> </li>
                 <li> <a target="_blank" rel="noreferrer" id="twitter" href="https://twitter.com/SachinP40"> <FaTwitter /></a> </li>
 
             </ul>
