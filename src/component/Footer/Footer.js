3fe06e2354e8ac0db903e903e77f9b95825d0021
Sachin Prajapati <53299703+sachinprajapati8604@users.noreply.github.com>
2021-08-30 20:38:44 +0530

    footer updated

--- src/component/Footer/Footer.js
+++ src/component/Footer/Footer.js
@@ -1,9 +1,20 @@
 import React from 'react'
 import './Footer.css'
+import { FaLinkedinIn, FaFacebookF, FaGithub, FaInstagram, FaTwitter } from 'react-icons/fa';
+
+
 export default function Footer() {
     return (
         <div className="footer-box">
-           <h5> Made by Sachin Prajapati</h5>
+            <h5> Follow me on</h5>
+            <ul className="icon-list">
+                <li> <a target="_blank" id="linkedin" href="https://www.linkedin.com/in/sachin-prajapati-8b1993188/" ><FaLinkedinIn /></a> </li>
+                <li> <a target="_blank" id="github" href="https://github.com/sachinprajapati8604"><FaGithub /> </a> </li>
+                <li> <a target="_blank" id="facebook" href="https://www.facebook.com/bullet.raja.1998"><FaFacebookF />  </a> </li>
+                <li> <a target="_blank" id="insta" href="https://www.instagram.com/prajapati_rangbaz/?r=nametag">  <FaInstagram /></a> </li>
+                <li> <a target="_blank" id="twitter" href="https://twitter.com/SachinP40"> <FaTwitter /></a> </li>
+
+            </ul>
         </div>
     )
 }
